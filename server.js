67d5084a2f492bb89e7978deb17b5eeb4265aa4a
luConcha <lucy.cm9@gmail.com>
2023-11-06 16:47:35 -0600

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
